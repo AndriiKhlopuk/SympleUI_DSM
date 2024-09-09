5a2e13c508fd2615232422b72bbaee55c8892bd1
AndriiKhlopuk <hlopuk@gmail.com>
2024-09-09 15:34:02 +0300

    change

--- DSM.py
+++ DSM.py
@@ -1,1 +1,14 @@
-#test
+import json
+
+if not hashMap.containsKey("a"):
+    hashMap.put("a", "")
+if not hashMap.containsKey("b"):
+    hashMap.put("b", "")
+return hashMap
+
+
+def sample1_on_input(hashMap, _files=None, _data=None):
+    if hashMap.get("listener") == "btn_res":
+        hashMap.put("toast", str(int(hashMap.get("a")) + int(hashMap.get("b"))))
+
+    return hashMap
