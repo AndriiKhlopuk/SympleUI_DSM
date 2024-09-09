76a547544c4e82c0c3e97990c3640bd459909ccc
AndriiKhlopuk <hlopuk@gmail.com>
2024-09-09 16:19:22 +0300

    change

--- DSM.py
+++ DSM.py
@@ -1,10 +1,11 @@
 import json
 
-if not hashMap.containsKey("a"):
-    hashMap.put("a", "")
-if not hashMap.containsKey("b"):
-    hashMap.put("b", "")
-return hashMap
+def sample1_on_create(hashMap,_files=None,_data=None):
+    if not hashMap.containsKey("a"):
+        hashMap.put("a", "")
+    if not hashMap.containsKey("b"):
+        hashMap.put("b", "")
+    return hashMap
 
 
 def sample1_on_input(hashMap, _files=None, _data=None):
